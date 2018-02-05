fae83b9be8869cc4aa51fb472600436c647a7a38
zuodong@baihe <zuodong@baihe.com>
2018-02-05 19:11:41 +0800

    kotlin练习

---
+++ src/main/kotlin/com/zuo/ko/HelloKotlin.kt
@@ -0,0 +1,19 @@
+package com.zuo.ko
+
+import rx.Observable
+import java.io.File
+import java.util.concurrent.Executors
+
+fun main(args: Array<String>) {
+    val text = File(ClassLoader.getSystemResource("input").path).readText()
+    Observable.from(text.toCharArray().asIterable()).filter { !it.isWhitespace() }.groupBy { it }.map{
+        o -> o.count().subscribe {
+           println("${o.key} -> $it")
+        }
+    }.subscribe()
+
+    val run = Executors.newCachedThreadPool()
+    run.execute {
+        println("helloWorld!")
+    }
+}
